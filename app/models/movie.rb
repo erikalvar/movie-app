f347d46e4457904d79e72619465f9282eab809cf
erikalvar <erikalvarbass@gmail.com>
2020-06-15 10:57:38 -0400

    adds users, authentication, and authorization

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
   validates :year, numericality: {greater_than: 1887}
-  validates :plot, length: {minimum: 10}
+  # validates :plot, length: {minimum: 10}
 
   has_many :actors
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,16 @@ Rails.application.routes.draw do
     post "/actors" => "actors#create"
     patch "/actors/:id" => "actors#update"
     delete "/actors/:id" => "actors#destroy"
-  end
-
-  namespace :api do
+ 
     get "/movies" => "movies#index"
     get "/movies/:id" => "movies#show"
     post "/movies" => "movies#create"
     patch "/movies/:id" => "movies#update"
     delete "/movies/:id" => "movies#destroy"
+
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
+
   end
 end
